4b3d8a071b8f4a36033e2b41e3348001de50fd43
anan048 <104295575+anan048@users.noreply.github.com>
2022-11-02 22:22:47 +0700

    Update LE.go

--- LE.go
+++ LE.go
@@ -7,130 +7,132 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
 type MyArgs struct {
 }
 
-type Arith struct {
+type Raft struct {
 }
 
 var vote float64 = 0
-var a int
+var timer int
 var term int = 0
-var terml int
-var leade bool = false
 var app bool = false
 var jumlahanggota float64 = 1
 
-func leader() {
-	leade = true
-	fmt.Printf("jadi leader term: %d", term)
+func append(peerAddress string, state int) bool {
+	client, err := rpc.DialHTTP("tcp", peerAddress)
+	handleConnError(err, peerAddress, state)
+	clientargs := term
+	var localResult bool
+	err = client.Call("Raft.AppendEntries", clientargs, &localResult)
+	handleConnError(err, peerAddress, state)
+	return localResult
+}
+
+func leader(peerAddress map[int]string) {
+	var result bool
+	state := 2
+	fmt.Printf("jadi leader. term: %d", term)
 	for true {
+		for i := 0; i < len(os.Args)-2; i++ {
+			result = append(peerAddress[i], state)
+		}
+		if result == true {
+			time.Sleep(10 * time.Second)
+		} else {
+			leader(peerAddress)
+		}
+
 	}
 }
 
-func candidate(peerAddress string) {
+func voting(peerAddress string, state int) bool {
+	client, err := rpc.DialHTTP("tcp", peerAddress)
+	handleConnError(err, peerAddress, state)
+	clientargs := ""
+	var localResult bool
+	err = client.Call("Raft.RequestVote", clientargs, &localResult)
+	handleConnError(err, peerAddress, state)
+	return localResult
+}
+
+func candidate(peerAddress map[int]string) {
+	var result bool
+	state := 1
 	for true {
 		fmt.Println("jadi candidate")
 		term += 1
 		vote += 1
-		client, err := rpc.DialHTTP("tcp", peerAddress)
-		handleConnError(err, peerAddress)
-		clientargs := ""
-		var localResult bool
-		err = client.Call("Arith.RequestVote", clientargs, &localResult)
-		result := localResult
-		if result == true {
-			jumlahanggota += 1
-			vote += 1
-			if vote > 0.5*jumlahanggota {
-				leader()
+		for i := 0; i < len(os.Args)-2; i++ {
+			result = voting(peerAddress[i], state)
+			if result == true {
+				jumlahanggota += 1
+				vote += 1
 			}
+		}
+
+		if vote > 0.5*jumlahanggota {
+			leader(peerAddress)
 		}
 	}
 
 }
 
-func (t *Arith) RequestVote(empty string, result *bool) error {
+func (t *Raft) RequestVote(empty string, result *bool) error {
 	*result = true
 	return nil
 }
 
-func (t *Arith) AppendEntries(termm *int, result *string) error {
+func (t *Raft) AppendEntries(termm int, result *bool) error {
+	
+	term = termm
+	if term == termm {
 
-	var b int
-
-	if leade == true {
-		b = term
+		app = true
 	}
-	s1 := strconv.Itoa(b)
-	s2 := strconv.FormatBool(leade)
-	*result = fmt.Sprint(s1 + "," + s2)
+	*result = true
 	return nil
 }
 
-func append(peerAddress string) {
-	client, err := rpc.DialHTTP("tcp", peerAddress)
-	handleConnError(err, peerAddress)
-	var split []string
-	for true {
-
-		clientargs := terml
-		var localResult string
-		err = client.Call("Arith.AppendEntries", clientargs, &localResult)
-		handleConnError(err, peerAddress)
-		split = strings.Split(localResult, ",")
-
-		leade, err = strconv.ParseBool(split[1])
-		if leade == true {
-			app = true
-			term, err = strconv.Atoi(split[0])
-		}
-		if app == true {
-			time.Sleep(10 * time.Second)
-		}
-
-	}
-}
-
-func follower(peerAddress string) {
-	go append(peerAddress)
+func follower(peerAddress map[int]string) {
+	rand.Seed(time.Now().UnixNano())
+	timer = rand.Intn(20) + 30
 	for true {
 
 		if app == true {
 			fmt.Println("menerima heartbeat")
-			a = rand.Intn(20) + 30
+			timer = rand.Intn(20) + 30
 			app = false
 		}
-		fmt.Printf("%d term: %d\n", a, term)
+		fmt.Printf("%d term: %d\n", timer, term)
 		time.Sleep(1 * time.Second)
-		a -= 1
+		timer -= 1
 
-		if a == 0 {
+		if timer == 0 {
 			candidate(peerAddress)
 		}
 	}
-
 }
 
 func main() {
 	portNumber := os.Args[1]
-	peersAddress := os.Args[2]
+	var peersAddress = make(map[int]string)
 
-	rand.Seed(time.Now().UnixNano())
-	a = rand.Intn(20) + 30
+	for i := 0; i < len(os.Args)-2; i++ {
+		peersAddress[i] = os.Args[i+2]
+	}
+
 	go follower(peersAddress)
-	// Inisiasi struct arith
-	arith := &Arith{}
+	// Inisiasi struct raft
+	raft := &Raft{}
 	// Registrasikan struct dan method ke RPC
-	rpc.Register(arith)
+	rpc.Register(raft)
 	// Deklarasikan bahwa kita menggunakan protokol HTTP sebagai msekanisme pengiriman pesan
 	rpc.HandleHTTP()
-	// Deklarasikan listerner HTTP dengan layer transport TCP dan Port 1234
+	// Deklarasikan listerner HTTP dengan layer transport TCP dan Port 
 	listener, err := net.Listen("tcp", ":"+string(portNumber))
 	handleError(err)
 	// Jalankan server HTTP
@@ -146,11 +148,24 @@ func handleError(err error) {
 	}
 }
 
-func handleConnError(err error, peersAdrress string) {
+func handleConnError(err error, peersAdrress string, state int) {
+	var a bool
 	if err != nil {
 		fmt.Println("Terdapat error : ", err.Error())
-		time.Sleep(1 * time.Second)
-		follower(peersAdrress)
+
+		time.Sleep(5 * time.Second)
+		if state == 1 {
+			a = voting(peersAdrress, state)
+			if a == true {
+				fmt.Println("")
+			}
+		} else {
+			a = append(peersAdrress, state)
+			if a == true {
+				fmt.Println("")
+			}
+		}
+
 	}
 
 }
